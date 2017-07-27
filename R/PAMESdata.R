08af83c39af6c003ec472aea44733f92db37b8f0
romagnolid <dario.romagnoli87@gmail.com>
2017-07-27 12:38:24 +0200

    first commit

---
+++ R/PAMESdata.R
@@ -0,0 +1,8 @@
+#' Precomputed list of informative CpG sites and CpG islands
+#'
+#' Using data from TCGA we copmute the sets of inforamtive CpG sites for 14 cancer.
+#' I set were computed using the function `select_informative_sites`
+#' from PAMES package.
+#'
+#' @seealso \url{https://github.com/romagnolid/PAMES}
+"_PACKAGE"
---
+++ R/data.R
@@ -0,0 +1,240 @@
+#' Bladder Urothelial Carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"BLCA_islands"
+
+#' Breast invasive carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"BRCA_islands"
+
+#' Colon adenocarcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"COAD_islands"
+
+#' Esophageal carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"ESCA_islands"
+
+#' Head and Neck squamous cell carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"HNSC_islands"
+
+#' Kidney renal clear cell carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"KIRC_islands"
+
+#' Kidney renal papillary cell carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"KIRP_islands"
+
+#' Liver hepatocellular carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"LIHC_islands"
+
+#' Lung adenocarcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"LUAD_islands"
+
+#' Lung squamous cell carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"LUSC_islands"
+
+#' Pancreatic adenocarcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"PAAD_islands"
+
+#' Prostate adenocarcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"PRAD_islands"
+
+#' Thyroid carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"THCA_islands"
+
+#' Uterine Corpus Endometrial Carcinoma
+#'
+#' A list of informative CpG islands to be used with `compute_purity` from PAMES
+#' package. Use with data from Bisulphite Sequencing after processing with
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"UCEC_islands"
+
+#-------------------------------------------------------------------------------
+#' Bladder Urothelial Carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"BLCA_sites"
+
+#' Breast invasive carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"BRCA_sites"
+
+#' Colon adenocarcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"COAD_sites"
+
+#' Esophageal carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"ESCA_sites"
+
+#' Head and Neck squamous cell carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"HNSC_sites"
+
+#' Kidney renal clear cell carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"KIRC_sites"
+
+#' Kidney renal papillary cell carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"KIRP_sites"
+
+#' Liver hepatocellular carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"LIHC_sites"
+
+#' Lung adenocarcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"LUAD_sites"
+
+#' Lung squamous cell carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"LUSC_sites"
+
+#' Pancreatic adenocarcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#' `reduce_bs_data`.
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"PAAD_sites"
+
+#' Prostate adenocarcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"PRAD_sites"
+
+#' Thyroid carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"THCA_sites"
+
+#' Uterine Corpus Endometrial Carcinoma
+#'
+#' A list of informative CpG sites to be used with `compute_purity` from PAMES
+#' package. Use with data from Illumina BeadChip
+#'
+#' @format A list of two vector of indexes (`hyper`- and `hypo`-methylated).
+"UCEC_sites"
+
