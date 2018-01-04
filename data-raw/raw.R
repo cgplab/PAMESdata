ab02e7adb19715ba212f3ffb81fba28285279586
romagnolid <dario.romagnoli87@gmail.com>
2018-01-04 10:26:00 +0100

    updated cpg_islands (including only islands with at least 3 sites with at most 50% of NAs)

--- data-raw/raw.R
+++ data-raw/raw.R
@@ -1,7 +1,8 @@
 library(dplyr)
+load("data-raw/cpg_islands_indexes.rda")
 all_sites <- readRDS("data-raw/cpg_sites.RDS")
-all_islands <- readRDS("data-raw/cpg_islands.RDS")
 tumors <- sort(names(all_sites))
+tumor <- "BRCA"
 for (tumor in tumors){
     hyper_sites <- all_sites[[tumor]] %>%
         dplyr::filter(type=="hyper" & n <= 10) %>%
@@ -14,16 +15,7 @@ for (tumor in tumors){
     sites <- list(hyper=hyper_sites, hypo=hypo_sites)
     assign(sprintf("%s_sites", tumor), sites)
 
-    hyper_islands <- all_islands[[tumor]] %>%
-        filter(type=="hyper" & n <= 10) %>%
-        select(index) %>%
-        unlist(use.names=F)
-    hypo_islands <- all_islands[[tumor]] %>%
-        filter(type=="hypo" & n <= 10) %>%
-        select(index) %>%
-        unlist(use.names=F)
-    islands <- list(hyper=hyper_islands, hypo=hypo_islands)
-    assign(sprintf("%s_islands", tumor), islands)
+    assign(sprintf("%s_islands", tumor), cpg_islands_indexes[[tumor]])
 }
 devtools::use_data(overwrite=T,
     BLCA_sites,
