b8c28cdf0884813d989f40d74e033ef920db8326
Dario Romagnoli <dario.romagnoli87@gmail.com>
2019-09-12 12:14:49 +0200

    Added AUC data

--- R/data.R
+++ R/data.R
@@ -238,3 +238,116 @@
 #' @format A list of two vector of indexes (\code{hyper}- and \code{hypo}-methylated).
 "UCEC_islands"
 
+#-------------------------------------------------------------------------------
+#' Bladder Urothelial Carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"BLCA_auc"
+
+#' Breast invasive carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"BRCA_auc"
+
+#' Colon adenocarcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"COAD_auc"
+
+#' Esophageal carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"ESCA_auc"
+
+#' Head and Neck squamous cell carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"HNSC_auc"
+
+#' Kidney renal clear cell carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"KIRC_auc"
+
+#' Kidney renal papillary cell carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"KIRP_auc"
+
+#' Liver hepatocellular carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"LIHC_auc"
+
+#' Lung adenocarcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"LUAD_auc"
+
+#' Lung squamous cell carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"LUSC_auc"
+
+#' Pancreatic adenocarcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"PAAD_auc"
+
+#' Prostate adenocarcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"PRAD_auc"
+
+#' Thyroid carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"THCA_auc"
+
+#' Uterine Corpus Endometrial Carcinoma
+#'
+#' A vector of AUC computed with \code{\link[PAMES]{compute_AUC}} from PAMES
+#' package. 
+#'
+#' @format A named numeric vector.
+"UCEC_auc"
+
